70ef3a93f4b35f14cafd71bcdd5147c29fbcf557
mechmank <mechmanktest@gmail.com>
2025-07-28 11:58:36 +0530

    v3

--- web_monitor.sh
+++ web_monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # check if httpd or nginx is installed
 if 
-    systemctl list-units --type=service | grep -q httpd; 
+    systemctl list-units --type=service --all | grep -q httpd; 
     then 
     service="httpd"
 elif 
-    systemctl list-units --type=service | grep -q nginx; 
+    systemctl list-units --type=service --all | grep -q nginx; 
     then 
     service="nginx"
 else
@@ -16,7 +16,7 @@ fi
 # Check if running
 if ! systemctl is-active --quiet $service; then
     systemctl start  $service
-    echo "$(date) : $service was down" >> /var/log/web_service_monitor.log
+    echo "$(date) : $service was down and restarted" >> /var/log/web_service_monitor.log
 else
     echo "$(date) : $service is running fine" >> /var/log/web_service_monitor.log
 fi
